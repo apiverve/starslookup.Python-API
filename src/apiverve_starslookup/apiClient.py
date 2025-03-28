01e9a481ed731e18c6d3d7ba014e7d69f637b52c
APIVerve Team <hello@apiverve.com>
2025-03-28 17:51:52 -0500

    Bug Fixes

--- src/apiverve_starslookup/apiClient.py
+++ src/apiverve_starslookup/apiClient.py
@@ -10,10 +10,10 @@ class StarslookupAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/starslookup'
+        self.base_url = 'https://api.apiverve.com/v1/starslookup'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
