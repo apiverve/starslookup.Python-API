514c0a559c1074b58ec1e331b45671ea7a972574
APIVerve Team <hello@apiverve.com>
2025-03-28 17:51:54 -0500

    Bug Fixes

--- src/apiverve_starslookup/setup.py
+++ src/apiverve_starslookup/setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_starslookup',
-    version='1.0.9',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
-    description='Stars Lookup is a simple tool for looking up star data. It returns the star name, constellation, and more.',
+    install_requires=install_requires,
+    description='Stars Lookup is a simple tool for looking up star data. It returns the star name, mass, diameter, and more.',
     author='APIVerve',
     author_email='hello@apiverve.com',
     url='https://apiverve.com',
